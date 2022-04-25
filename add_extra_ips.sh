b888f6083f5ca660ec47106f94d60fe3345a3dc7
WGH <wgh@torlan.ru>
2022-04-25 16:51:48 +0300

    Improve add_extra_ips.sh script

--- add_extra_ips.sh
+++ add_extra_ips.sh
@@ -2,6 +2,8 @@
 
 INTERFACE="$(ip -json route get :: | jq -r '.[0].dev')"
 ADDR="$(ip -json address show dev ${INTERFACE} | jq -r '.[0].addr_info[] | select(.prefixlen == 64 and .mngtmpaddr and .family == "inet6").local')"
+VALIDITY=$(( 3600 * 24 * 365 ))
+NUM_ADDRESSES=128
 
 gen_addresses() {
     python3 -c '
@@ -16,7 +18,7 @@ while True:
 ' "$1"
 }
 
-gen_addresses "$ADDR" | head -n 16 | while read addr; do
+gen_addresses "$ADDR" | head -n "$NUM_ADDRESSES" | while read addr; do
     echo "Adding $addr..." >&2
-    ip addr add "$addr/64" dev "$INTERFACE" valid_lft 3600 preferred_lft 0
+    ip addr add "$addr/64" dev "$INTERFACE" valid_lft "$VALIDITY" preferred_lft 0
 done
